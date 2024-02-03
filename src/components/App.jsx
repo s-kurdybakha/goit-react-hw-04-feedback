9e5bb3bf146adadae65e06000609df8adeec4581
Sergii Kurdybakha <s.kurdybakha@gmail.com>
2024-02-03 20:06:18 +0200

    refactoring from class by hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,131 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const handleIncrementRewiew = e => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [e.target.name]: prevFeedback[e.target.name] + 1,
+    }));
   };
 
-  handleIncrementRewiew = e => {
-    this.setState(prevState => {
-      return { [e.target.name]: prevState[e.target.name] + 1 };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = feedback.good + feedback.neutral + feedback.bad;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = () => {
     return Math.round(
-      (this.state.good /
-        (this.state.good + this.state.neutral + this.state.bad)) *
-        100
+      (feedback.good / (feedback.good + feedback.neutral + feedback.bad)) * 100
     );
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-          // backgroundColor: '#e7ecf2',
-        }}
-      >
-        {/* React homework template */}
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleIncrementRewiew}
+  const { good, neutral, bad } = feedback;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        // backgroundColor: '#e7ecf2',
+      }}
+    >
+      {/* React homework template */}
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={handleIncrementRewiew}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleIncrementRewiew = e => {
+//     this.setState(prevState => {
+//       return { [e.target.name]: prevState[e.target.name] + 1 };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     const total = this.state.good + this.state.neutral + this.state.bad;
+//     return total;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round(
+//       (this.state.good /
+//         (this.state.good + this.state.neutral + this.state.bad)) *
+//         100
+//     );
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <div
+//         style={{
+//           height: '100vh',
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           fontSize: 40,
+//           color: '#010101',
+//           // backgroundColor: '#e7ecf2',
+//         }}
+//       >
+//         {/* React homework template */}
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.handleIncrementRewiew}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
 export default App;
