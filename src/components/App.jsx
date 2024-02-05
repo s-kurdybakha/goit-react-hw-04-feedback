ae9981a2622cfb5254ce4a28a5c4cd20e7194e47
Sergii Kurdybakha <s.kurdybakha@gmail.com>
2024-02-05 19:11:06 +0200

    removed commented code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,7 +38,6 @@ const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
-        // backgroundColor: '#e7ecf2',
       }}
     >
       {/* React homework template */}
@@ -65,67 +64,4 @@ const App = () => {
   );
 };
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrementRewiew = e => {
-//     this.setState(prevState => {
-//       return { [e.target.name]: prevState[e.target.name] + 1 };
-//     });
-//   };
-
-//   countTotalFeedback = () => {
-//     const total = this.state.good + this.state.neutral + this.state.bad;
-//     return total;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round(
-//       (this.state.good /
-//         (this.state.good + this.state.neutral + this.state.bad)) *
-//         100
-//     );
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <div
-//         style={{
-//           height: '100vh',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           fontSize: 40,
-//           color: '#010101',
-//           // backgroundColor: '#e7ecf2',
-//         }}
-//       >
-//         {/* React homework template */}
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.handleIncrementRewiew}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
 export default App;
